662d1a80e0f1a1ee294ad9699368ad1ca208128c
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-02-14 15:13:35 -0300

    Refactored Enrollments table

--- src/main/resources/db/migration/V6__create_table_enrollments.sql
+++ src/main/resources/db/migration/V6__create_table_enrollments.sql
@@ -1,7 +1,8 @@
 CREATE TABLE enrollments (
-    id SERIAL PRIMARY KEY,
     student_id BIGINT NOT NULL,
     class_id BIGINT NOT NULL,
+
+    PRIMARY KEY (student_id, class_id),
 
     CONSTRAINT fk_enrollment_student
         FOREIGN KEY (student_id)
