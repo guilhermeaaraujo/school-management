d49049f463c17aa41adadc8d98875b294cb64921
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-03-01 16:13:50 -0300

    Documentation for Teacher and Subject endpoints

--- src/main/java/com/guilherme/schoolmanagement/controllers/SubjectController.java
+++ src/main/java/com/guilherme/schoolmanagement/controllers/SubjectController.java
@@ -1,8 +1,13 @@
 package com.guilherme.schoolmanagement.controllers;
 
+import com.guilherme.schoolmanagement.config.SecurityConfig;
 import com.guilherme.schoolmanagement.domain.entities.Subject;
 import com.guilherme.schoolmanagement.domain.dto.response.SubjectDTO;
 import com.guilherme.schoolmanagement.services.SubjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/subjects")
+@Tag(name = "subjects", description = "Controlador para recuperar/alterar dados de disciplinas")
+@SecurityRequirement(name = SecurityConfig.SECURITY)
 public class SubjectController {
 
     @Autowired
@@ -21,6 +28,10 @@ public class SubjectController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
+    @Operation(summary = "Listar disciplinas", description = "Lista todas as disciplinas do banco de dados")
+    @ApiResponse(responseCode = "200", description = "Disciplinas retornadas com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<List<SubjectDTO>> findAll() {
         List<Subject> subjects = service.findAll();
         List<SubjectDTO> subjectsDTO = subjects.stream().map(x -> new SubjectDTO(x)).toList();
@@ -30,6 +41,10 @@ public class SubjectController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
+    @Operation(summary = "Listar disciplina por id", description = "Retorna uma disciplina por id")
+    @ApiResponse(responseCode = "200", description = "Disciplina retornada com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<SubjectDTO> findById(@PathVariable Long id) {
         Subject subject = service.findById(id);
         return ResponseEntity.ok().body(new SubjectDTO(subject));
@@ -37,6 +52,10 @@ public class SubjectController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
+    @Operation(summary = "Criar disciplina", description = "Insere uma nova disciplina no banco de dados")
+    @ApiResponse(responseCode = "201", description = "Disciplina criada com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<SubjectDTO> insert(@RequestBody Subject subject) {
         service.insert(subject);
         return ResponseEntity.status(HttpStatus.CREATED).body(new SubjectDTO(subject));
@@ -44,6 +63,10 @@ public class SubjectController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar disciplina", description = "Deleta uma disciplina do banco de dados")
+    @ApiResponse(responseCode = "200", description = "Disciplina deletada com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
@@ -51,6 +74,10 @@ public class SubjectController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar disciplina", description = "Deleta uma disciplina do banco de dados")
+    @ApiResponse(responseCode = "200", description = "Disciplina atualizada com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<SubjectDTO> update(@PathVariable Long id, @RequestBody Subject subject) {
         subject = service.update(id, subject);
         return ResponseEntity.ok().body(new SubjectDTO(subject));
--- src/main/java/com/guilherme/schoolmanagement/controllers/TeacherController.java
+++ src/main/java/com/guilherme/schoolmanagement/controllers/TeacherController.java
@@ -1,5 +1,6 @@
 package com.guilherme.schoolmanagement.controllers;
 
+import com.guilherme.schoolmanagement.config.SecurityConfig;
 import com.guilherme.schoolmanagement.domain.dto.request.EntityRequestDTO;
 import com.guilherme.schoolmanagement.domain.entities.Student;
 import com.guilherme.schoolmanagement.domain.entities.Teacher;
@@ -7,6 +8,10 @@ import com.guilherme.schoolmanagement.domain.dto.response.TeacherDTO;
 import com.guilherme.schoolmanagement.domain.entities.User;
 import com.guilherme.schoolmanagement.domain.enums.UserRole;
 import com.guilherme.schoolmanagement.services.TeacherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +23,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/teachers")
+@Tag(name = "teachers", description = "Controlador para recuperar/alterar dados de professores")
+@SecurityRequirement(name = SecurityConfig.SECURITY)
 public class TeacherController {
 
     @Autowired
@@ -25,6 +32,10 @@ public class TeacherController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
+    @Operation(summary = "Listar professores", description = "Lista todos os professores do banco de dados")
+    @ApiResponse(responseCode = "200", description = "Professores Retornados com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<List<TeacherDTO>> findAll() {
         List<Teacher> teachers = service.findAll();
         List<TeacherDTO> teachersDTO = teachers.stream().map(x -> new TeacherDTO(x)).toList();
@@ -34,6 +45,10 @@ public class TeacherController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
+    @Operation(summary = "Listar professor por id", description = "Retorna um professor por id")
+    @ApiResponse(responseCode = "200", description = "Professor retornado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<TeacherDTO> findById(@PathVariable Long id) {
         Teacher teacher = service.findById(id);
         return ResponseEntity.ok().body(new TeacherDTO(teacher));
@@ -41,6 +56,10 @@ public class TeacherController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
+    @Operation(summary = "Criar professor", description = "Insere um novo professor no banco de dados")
+    @ApiResponse(responseCode = "201", description = "Professor criado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<TeacherDTO> insert(@RequestBody EntityRequestDTO requestDTO) {
         Teacher teacher = new Teacher(requestDTO.firstName(), requestDTO.lastName(), requestDTO.birthDate(), new User(requestDTO.email(), null, UserRole.TEACHER));
         teacher = service.insert(teacher);
@@ -49,6 +68,10 @@ public class TeacherController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar professor", description = "Deleta um professor do banco de dados")
+    @ApiResponse(responseCode = "200", description = "Professor deletado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
@@ -56,6 +79,10 @@ public class TeacherController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar professor", description = "Deleta um professor do banco de dados")
+    @ApiResponse(responseCode = "200", description = "Professor atualizado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Usuário nao tem acesso à operação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<TeacherDTO> update(@PathVariable Long id, @RequestBody EntityRequestDTO requestDTO) {
         Teacher teacher = new Teacher(requestDTO.firstName(), requestDTO.lastName(), requestDTO.birthDate(), new User(requestDTO.email(), UserRole.TEACHER));
         teacher = service.update(id, teacher);
